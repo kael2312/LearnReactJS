46e2d9d983c0f832048394d6aa63eddbcd1964ab
kael2312 <lenhathoang2311@gmail.com>
2022-04-14 08:45:45 +0700

    commit demo state

---
+++ Section 06 - State/section-06/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Color from "./components/Color/color";
+import Counter from "./components/Counter/counter";
+
+function App() {
+  return (
+    <div>
+      <Color></Color>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
