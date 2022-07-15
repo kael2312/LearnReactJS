50a80819ad9330c6230d26c350e6dd8cf356eb85
hoangln <hoangln@mor.com.vn>
2022-07-15 15:50:01 +0700

    show thubmnail and format price

--- Section 14.19 - EcommerceApp/ecommerce-app/src/features/Product/components/ProductCard/product-card.jsx
+++ Section 14.19 - EcommerceApp/ecommerce-app/src/features/Product/components/ProductCard/product-card.jsx
@@ -8,6 +8,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Box, makeStyles } from "@material-ui/core";
+import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from "constants/common.constants";
 
 ProductCard.propTypes = {
     product: PropTypes.object,
@@ -22,17 +23,23 @@ const useStyles = makeStyles({
     },
 });
 
-function ProductCard(props) {
-    const { product } = props;
+function ProductCard({ product }) {
     const classes = useStyles();
+    const thumbnail = product.thumbnail?.url ? STATIC_HOST + `${product.thumbnail.url}` : THUMBNAIL_PLACEHOLDER
 
     return (
         <Box className={classes.root}>
+            <Box padding={1}>
+                <img src={thumbnail} alt={product.name} width="100%" />
+            </Box>
             <Typography gutterBottom variant="h5" component="h2">
                 {product.name}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-                {product.originalPrice}
+                <Box component="span" fontSize="16px" fontWeight="bold" mr={1}>
+                    {new Intl.NumberFormat('vi-VN', {style: 'currency', currency: 'VND'}).format(product.salePrice)}
+                </Box>
+                {product.promotionPercent > 0 ? ` -${product.promotionPercent}%` : ''}
             </Typography>
         </Box>
     );
